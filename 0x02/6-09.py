c5ab6dedfab3955daefc44cebd1358c156b1598b
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2020-09-10 16:42:02 -0500

    Exercises 0x02

--- 0x02/6-09.py
+++ 0x02/6-09.py
@@ -19,6 +19,7 @@ def mcd(a, b):
     print(f'El máximo común divisor (MCD) de {a} y {b} es: {res}')
 
 
+mcd(4, 6)
 mcd(18, 24)
 
 
@@ -39,10 +40,58 @@ def mcm(a, b):
 
 
 mcm(4, 6)
+mcm(18, 24)
 
-"""
-def gcf(a, b)
+
+def primeNumber(number):
+    countFactors = 0
+    for factor in range(1, number + 1):
+        if number % factor == 0:
+            countFactors += 1
+    if countFactors == 2:
+        return True
+    else:
+        return False
+
+
+def primeNumberList(limit):
+    primeList = []
+    for number in range(2, limit + 1):
+        if primeNumber(number) == True:
+            primeList.append(number)
+    return (primeList)
+
+
+def gcf(a, b):
+    primeNumbers = primeNumberList(10)
+    factorsList = []
+    for prime in primeNumbers:
+        if a % prime == 0 and b % prime == 0:
+            factorsList.append(prime)
+    res = 1
+    for factor in factorsList:
+        res *= factor
+    print('The Greatest Common Factor (GCF) the {} and {} is: {}'.format(a, b, res))
+
+
+gcf(4, 6)
 gcf(18, 24)
-def lcm(a, b)
+
+
+def lcm(a, b):
+    primeNumbers = primeNumberList(10)
+    factorList = []
+    for prime in primeNumbers:
+        if a % prime == 0:
+            factorList.append(prime)
+        if b % prime == 0:
+            factorList.append(prime)
+        res = 1
+        print(factorList)
+        for factor in factorList:
+            res *= factor
+    print('The Least Common Multiple (LCM) the {} and {} is: {}'.format(a, b, res))
+
+
 lcm(4, 6)
-"""
+lcm(18, 24)
