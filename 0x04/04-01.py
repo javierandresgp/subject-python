6a8bcf44d044b3debe5dc8c78ffbf77b57c27309
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-12-02 19:22:44 -0500

    Training

--- 0x04/04-01.py
+++ 0x04/04-01.py
@@ -7,6 +7,7 @@ def is_it_even(n):
         return True
     return False
 
+
 def is_it_prime(n):
     """ Dado un número entero n, indicar si es o no primo. """
     if n == 2:
@@ -15,13 +16,24 @@ def is_it_prime(n):
         return False
     for i in range(2, n):
         if n % i == 0:
-            print(n % i)
-#            return False
-#        return True
+            return False
+    return True
+
+
+def with_abs(n):
+    return abs(n)
+
+
+def without_abs(n):
+    """ Escribir una implementación propia de la función abs, que devuelva el
+    valor absoluto de cualquier valor que reciba """
+    if n < 0:
+        return n * - 1
+    return n
 
 
 if __name__ == "__main__":
-""" print(is_it_even(10))
+    print(is_it_even(10))
     print(is_it_even(21))
     print(is_it_prime(-1))
     print(is_it_prime(0))
@@ -33,4 +45,21 @@ if __name__ == "__main__":
     print(is_it_prime(6))
     print(is_it_prime(7))
     print(is_it_prime(8))
-""" print(is_it_prime(9))
+    print(is_it_prime(9))
+    print(is_it_prime(10))
+    print(is_it_prime(11))
+    print(is_it_prime(12))
+    print(is_it_prime(13))
+    print(is_it_prime(14))
+    print(is_it_prime(15))
+    print(is_it_prime(16))
+    print(with_abs(-2))
+    print(without_abs(-2))
+    print(with_abs(-1))
+    print(without_abs(-1))
+    print(with_abs(0))
+    print(without_abs(0))
+    print(with_abs(1))
+    print(without_abs(1))
+    print(with_abs(2))
+    print(without_abs(2))
